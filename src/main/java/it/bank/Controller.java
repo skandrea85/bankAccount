2b056bc8ab4b795cafd8177dd539d366a32eb073
andrea_falivene <andrea.falivene@gmail.com>
2021-10-06 17:56:40 +0200

    commit 3

--- src/main/java/it/bank/Controller.java
+++ src/main/java/it/bank/Controller.java
@@ -3,9 +3,20 @@ package it.bank;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties.RSocket.Client;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -16,6 +27,9 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -24,6 +38,7 @@ import com.squareup.okhttp.Response;
 import com.squareup.okhttp.ResponseBody;
 
 import kong.unirest.json.JSONObject;
+import okio.BufferedSink;
 @RestController
 public class Controller {
 
@@ -46,22 +61,56 @@ public class Controller {
 		return  new ResponseEntity<>(resStr, HttpStatus.OK) ;
 	}
 
-	@RequestMapping(value="/bonifico/{bankAccount}",produces="application/json")
-	public ResponseEntity<String> moneyTransfer(@PathVariable String bankAccount ) throws IOException{
+	@RequestMapping(value="/bonifico/{bankAccount}/{fiscalCodeBeneficiary}/{description}/{amount}/{date}",produces="application/json")
+	public ResponseEntity<String> moneyTransfer(@PathVariable String bankAccount,@PathVariable String fiscalCodeBeneficiary,@PathVariable String description,@PathVariable Number amount,@PathVariable String date) throws IOException, ParseException{
 
-		OkHttpClient client = new OkHttpClient();
-		MediaType mediaType = MediaType.parse("application/json");
-		RequestBody body = RequestBody.create(mediaType, "{\r\n  \"creditor\": {\r\n    \"name\": \"LUCA TERRIBILE\",\r\n    \"account\": {\r\n      \"accountCode\": \"IT40L0326822311052923800661\"\r\n      \r\n    },\r\n    \"address\": {\r\n      \"address\": null,\r\n      \"city\": null,\r\n      \"countryCode\": null\r\n    }\r\n  },\r\n  \"executionDate\": \"2019-04-01\",\r\n  \"uri\": \"REMITTANCE_INFORMATION\",\r\n  \"description\": \"Payment invoice 75/2017\",\r\n  \"amount\": 800,\r\n  \"currency\": \"EUR\",\r\n  \"isUrgent\": false,\r\n  \"isInstant\": false,\r\n  \"feeType\": \"SHA\",\r\n  \"feeAccountId\": \"45685475\",\r\n  \"taxRelief\": {\r\n    \"taxReliefId\": \"L449\",\r\n    \"isCondoUpgrade\": false,\r\n    \"creditorFiscalCode\": \"56258745832\",\r\n    \"beneficiaryType\": \"NATURAL_PERSON\",\r\n    \"naturalPersonBeneficiary\": {\r\n      \"fiscalCode1\": \"MRLFNC81L04A859L\",\r\n      \"fiscalCode2\": null,\r\n      \"fiscalCode3\": null,\r\n      \"fiscalCode4\": null,\r\n      \"fiscalCode5\": null\r\n    },\r\n    \"legalPersonBeneficiary\": {\r\n      \"fiscalCode\": null,\r\n      \"legalRepresentativeFiscalCode\": null\r\n    }\r\n  }\r\n}");
-		Request request = new Request.Builder()
-				.url("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+bankAccount+"/payments/money-transfers ")
-				.method("POST", body)
-				.addHeader("Auth-Schema", "S2S")
-				.addHeader("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP")
-				.addHeader("Content-Type", "application/json")
-				.build();
-		Response response = client.newCall(request).execute();
-		String res =response.body().string();
-		return  new ResponseEntity<>(res, HttpStatus.MULTI_STATUS) ;
+		MoneyTransfer moneyTransfer =new MoneyTransfer();
+
+		Creditor creditor =new Creditor();
+		Account account =new Account();
+
+		account.setAccountCode("IT40L0326822311052923800661");
+
+		creditor.setAccount(account);
+
+		creditor.setName("LUCA TERRIBILE");
+
+
+		moneyTransfer.setAmount(amount);
+		moneyTransfer.setCreditor(creditor);
+		moneyTransfer.setDescription(description);
+		moneyTransfer.setCurrency("EUR");
+
+
+		moneyTransfer.setExecutionDate(date);
+		TaxRelief taxRelief=new TaxRelief();
+		taxRelief.setBeneficiaryType("NATURAL_PERSON");
+		NaturalPersonBeneficiary nPerson=new NaturalPersonBeneficiary();
+		nPerson.setFiscalCode1(fiscalCodeBeneficiary);
+		taxRelief.setTaxReliefId("L449");
+		taxRelief.setIsCondoUpgrade(false);
+		taxRelief.setNaturalPersonBeneficiary(nPerson);
+		taxRelief.setCreditorFiscalCode("56258745832");
+		moneyTransfer.setTaxRelief(taxRelief);
+
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		String a= gson.toJson(moneyTransfer);
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		HttpPost httpPost = new HttpPost("https://sandbox.platfr.io/api/gbs/banking/v4.0/accounts/"+bankAccount+"/payments/money-transfers");
+
+
+		HttpEntity stringEntity = new StringEntity(a,ContentType.APPLICATION_JSON);
+		httpPost.setEntity(stringEntity);
+		httpPost.addHeader("Auth-Schema", "S2S");
+		httpPost.addHeader("Api-Key", "FXOVVXXHVCPVPBZXIJOBGUGSKHDNFRRQJP");
+		CloseableHttpResponse response2 = httpclient.execute(httpPost);
+		
+		String json = EntityUtils.toString(response2.getEntity());
+
+		return  new ResponseEntity<>(json, HttpStatus.MULTI_STATUS) ;
 
 	}
 
